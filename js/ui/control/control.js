1d1a1fe5adef4708f0183b0d7235f467d54dd6be
Lauren Budorick <lauren@mapbox.com>
2015-01-08 17:07:24 -0800

    rm debugger

--- js/ui/control/control.js
+++ js/ui/control/control.js
@@ -6,7 +6,6 @@ function Control() {}
 
 Control.prototype = {
 	addTo(map) {
-		debugger;
 		this._map = map;
 		this._container = this.onAdd(map);
 		if (!this.attrib) this._container.className += ' mapboxgl-ctrl-' + (this.opts && this.opts.position || 'topright');
